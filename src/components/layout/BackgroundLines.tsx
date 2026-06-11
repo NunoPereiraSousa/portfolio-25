2ded8c86cc3f09f6852f759af4ba5d8d2f63d714
Nuno Pereira Sousa <nunopereirasousa00@gmail.com>
2026-06-11 16:49:44 +0100

    remove horizontal lines on mobile/tablet

--- src/components/layout/BackgroundLines.tsx
+++ src/components/layout/BackgroundLines.tsx
@@ -25,14 +25,6 @@ export function MobileBackgroundLines() {
         <span></span>
         <span></span>
       </div>
-      <div className="webgl-mobile-lines__horizontal" aria-hidden="true">
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
     </div>
   );
 }
